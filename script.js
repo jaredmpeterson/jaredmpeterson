41f0706845689ec3a87b65241e04c668966c7ca7
Jared M. Peterson <jared@peteyinc.com>
2016-08-23 21:20:43 -0600

    fixed time display

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-var today = moment().format("ddd MMM Do h:mm A");
-document.getElementById('dandt').innerHTML=today;
+// var today = moment().format("ddd MMM Do h:mm A");
+// document.getElementById('dandt').innerHTML=today;
+
+(function () {
+
+  var dandt = document.getElementById('dandt');
+
+  function updateClock ( clock ) {
+    clock.innerHTML = moment().format("ddd MMM Do h:mm:ss A");
+  }
+
+  setInterval(function () {
+      updateClock( dandt );
+  }, 1000);
+
+}());
