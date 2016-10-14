ba198c34b06adda0d187223958f1e010d3e99233
jaredmpeterson <jared@peteyinc.com>
2016-10-14 16:56:56 -0600

    bower

--- gulpfile.js
+++ gulpfile.js
@@ -4,59 +4,69 @@ var config = require('./gulp.config')();
 var del = require('del');
 
 var $ = require('gulp-load-plugins')({
-    lazy: true
+  lazy: true
 });
 
 gulp.task('vet', function () {
-    log('Analyzing source with JSHint and JSCS');
+  log('Analyzing source with JSHint and JSCS');
 
-    gulp.src(config.alljs)
-        .pipe($.if(args.verbose, $.print()))
-        .pipe($.jscs())
-        .pipe($.jshint())
-        .pipe($.jshint.reporter('jshint-stylish', {
-            verbose: true
-        }))
-        .pipe($.jshint.reporter('fail'));
+  gulp.src(config.alljs)
+    .pipe($.if(args.verbose, $.print()))
+    .pipe($.jscs())
+    .pipe($.jshint())
+    .pipe($.jshint.reporter('jshint-stylish', {
+      verbose: true
+    }))
+    .pipe($.jshint.reporter('fail'));
 });
 
 gulp.task('styles', ['clean-styles'], function () {
-    log('Compiling SCSS --> CSS');
+  log('Compiling SCSS --> CSS');
 
-    return gulp
-        .src(config.sass)
-        .pipe($.plumber())
-        .pipe($.sass())
-        .pipe($.autoprefixer({
-            browsers: ['last 2 version', '> 5%']
-        }))
-        .pipe(gulp.dest(config.temp));
+  return gulp
+    .src(config.sass)
+    .pipe($.plumber())
+    .pipe($.sass())
+    .pipe($.autoprefixer({
+      browsers: ['last 2 version', '> 5%']
+    }))
+    .pipe(gulp.dest(config.temp));
 });
 
 gulp.task('clean-styles', function () {
-    var files = config.temp + '**/*.css';
-    clean(files);
+  var files = config.temp + '**/*.css';
+  clean(files);
 });
 
 gulp.task('style-watch', function () {
-    gulp.watch([config.sass], ['styles']);
+  gulp.watch([config.sass], ['styles']);
 });
 
+gulp.task('wiredep', function () {
+  var options = config.wiredepOptions();
+  var wiredep = require('wiredep').stream;
+
+  return gulp
+    .src(config.index)
+    .pipe(wiredep(options))
+    .pipe($.inject(gulp.src(config.js)))
+    .pipe(gulp.dest(config.client));
+});
 
 // Functions //
 function clean(path, done) {
-    log('Cleaning: ' + $.util.colors.blue(path));
-    del(path, done);
+  log('Cleaning: ' + $.util.colors.blue(path));
+  del(path, done);
 }
 
 function log(msg) {
-    if (typeof (msg) === 'object') {
-        for (var item in msg) {
-            if (msg.hasOwnProperty(item)) {
-                $.util.log($.util.colors.blue(msg[item]));
-            }
-        }
-    } else {
-        $.util.log($.util.colors.blue(msg));
+  if (typeof (msg) === 'object') {
+    for (var item in msg) {
+      if (msg.hasOwnProperty(item)) {
+        $.util.log($.util.colors.blue(msg[item]));
+      }
     }
+  } else {
+    $.util.log($.util.colors.blue(msg));
+  }
 }
--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-// var today = moment().format("ddd MMM Do h:mm A");
-// document.getElementById('dandt').innerHTML=today;
-
 (function () {
+  // var today = moment().format("ddd MMM Do h:mm A");
+  var dandt = document.getElementById('dandt');
+  // document.getElementById('dandt').innerHTML=today;
+  function updateClock(clock) {
+    clock.innerHTML = moment().format('ddd MMM Do h:mm:ss A');
+  }
 
-	var dandt = document.getElementById('dandt');
-
-	function updateClock(clock) {
-		clock.innerHTML = moment().format('ddd MMM Do h:mm:ss A');
-	}
-
-	setInterval(function () {
-		updateClock(dandt);
-	}, 1000);
+  setInterval(function () {
+    updateClock(dandt);
+  }, 1000);
 
 }());
 
-$('article').draggable({ handle: '.title' });
+$('article').draggable({
+  handle: '.title'
+});
