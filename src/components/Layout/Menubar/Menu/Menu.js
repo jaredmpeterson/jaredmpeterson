e81a482f8ab08ac899fb6b6888fcab9909eb07a4
Jared M. Peterson <developer@peteyinc.com>
2018-07-17 08:39:41 -0600

    fix menu

--- src/components/Layout/Menubar/Menu/Menu.js
+++ src/components/Layout/Menubar/Menu/Menu.js
@@ -6,34 +6,125 @@ const Menu = props => {
     <div className={classes.Menu}>
       <nav className={classes.Mainmenu}>
         <ul>
-          <i className="fa fa-apple" aria-hidden="true" />
           <li>
-            <p>JMP</p>
-            <ul className={classes.Submenu}>
+            <a href="#">
+              <i className="fa fa-apple" aria-hidden="true" />
+            </a>
+            <ul id="apple__menu">
               <li>
-                <p>About Jared</p>
+                <a href="#">About This Site</a>
               </li>
-              <li className={classes.Break} />
+              <li className={classes.Menubreak} />
               <li>
-                <p>Empty Trash...</p>
+                <a href="#">System Preferences...</a>
               </li>
-              <li className={classes.Break} />
               <li>
-                <p>Show All</p>
+                <a href="#">App Store...</a>
+              </li>
+              <li className={classes.Menubreak} />
+              <li>
+                <a href="#">Sleep</a>
+              </li>
+              <li>
+                <a href="#">Restart...</a>
+              </li>
+              <li>
+                <a href="#">Shut Down...</a>
+              </li>
+              <li className={classes.Menubreak} />
+              <li>
+                <a href="#">Log Out...</a>
               </li>
             </ul>
           </li>
           <li>
-            <p>File</p>
+            <a href="#">JMP</a>
+            <ul>
+              <li>
+                <a href="#">About Jared</a>
+              </li>
+              <li className={classes.Menubreak} />
+              {/* Easter Egg: Take out trash from Twins */}
+              <li>
+                <a href="#">Empty Trash...</a>
+              </li>
+              <li className={classes.Menubreak} />
+              <li>
+                <a href="#">Show All</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">File</a>
+            {/* <ul>
+              <li><a href="#">Sub Menu 1</a></li>
+              <li><a href="#">Sub Menu 2</a></li>
+              <li><a href="#">Sub Menu 3</a></li>
+              <li><a href="#">Sub Menu 4</a>
+                <ul>
+                  <li><a href="#">Deep Menu 1</a>
+                    <ul>
+                      <li><a href="#">Sub Deep 1</a></li>
+                      <li><a href="#">Sub Deep 2</a></li>
+                      <li><a href="#">Sub Deep 3</a></li>
+                      <li><a href="#">Sub Deep 4</a></li>
+                    </ul>
+                  </li>
+                  <li><a href="#">Deep Menu 2</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Sub Menu 5</a></li>
+            </ul> */}
           </li>
           <li>Edit</li>
           <li>View</li>
           <li>
-            <p>Go</p>
+            <a href="#">Go</a>
+            <ul>
+              <li>
+                <a
+                  href="https://github.com/jaredmpeterson"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-github fa-lg fa-fw" aria-hidden="true" />GitHub
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/thejaredpeterson"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i
+                    className="fa fa-linkedin fa-fw fa-lg"
+                    aria-hidden="true"
+                  />LinkedIn
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://twitter.com/jaredmpeterson"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <i className="fa fa-twitter fa-fw fa-lg" aria-hidden="true" />Twitter
+                </a>
+              </li>
+            </ul>
           </li>
           <li>Window</li>
           <li>
-            <p>Help</p>
+            <a href="#">Help</a>
+            <ul>
+              <li>
+                <a href="#">Help</a>
+              </li>
+              <li>
+                <a href="#">What's New in jmpOS</a>
+              </li>
+              {/* <li><a href="#"></a></li> */}
+            </ul>
           </li>
         </ul>
       </nav>
