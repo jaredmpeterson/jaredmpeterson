ed5aea92f4392323713ca3118057b2d8212f4822
Jared M. Peterson <developer@peteyinc.com>
2018-07-17 08:16:15 -0600

    menu

--- src/components/Layout/Menubar/Menu/Menu.js
+++ src/components/Layout/Menubar/Menu/Menu.js
@@ -3,39 +3,41 @@ import classes from './Menu.css'
 
 const Menu = props => {
   return (
-    <nav className={classes.Menu}>
-      <i className="fa fa-apple" aria-hidden="true" />
-      <ul className={classes.Mainmenu}>
-        <li>
-          <p>JMP</p>
-          <ul className={classes.Submenu}>
-            <li>
-              <p>About Jared</p>
-            </li>
-            <li className={classes.Break} />
-            <li>
-              <p>Empty Trash...</p>
-            </li>
-            <li className={classes.Break} />
-            <li>
-              <p>Show All</p>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <p>File</p>
-        </li>
-        <li>Edit</li>
-        <li>View</li>
-        <li>
-          <p>Go</p>
-        </li>
-        <li>Window</li>
-        <li>
-          <p>Help</p>
-        </li>
-      </ul>
-    </nav>
+    <div className={classes.Menu}>
+      <nav className={classes.Mainmenu}>
+        <ul>
+          <i className="fa fa-apple" aria-hidden="true" />
+          <li>
+            <p>JMP</p>
+            <ul className={classes.Submenu}>
+              <li>
+                <p>About Jared</p>
+              </li>
+              <li className={classes.Break} />
+              <li>
+                <p>Empty Trash...</p>
+              </li>
+              <li className={classes.Break} />
+              <li>
+                <p>Show All</p>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <p>File</p>
+          </li>
+          <li>Edit</li>
+          <li>View</li>
+          <li>
+            <p>Go</p>
+          </li>
+          <li>Window</li>
+          <li>
+            <p>Help</p>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
 }
 
